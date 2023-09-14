b04bf3e32ff44bc53b068d5b12e9ea268db9c4ca
kimanikevin254 <kimanikevin254@gmail.com>
2023-09-14 11:06:57 +0300

    fixed script

--- disk_space_monitor.py
+++ disk_space_monitor.py
@@ -1,22 +1,55 @@
 import psutil
 import requests
 import json
+import os
 
 def check_disk_space():
     disk = psutil.disk_usage('/var/lib/data')
     return {
-        "Used": disk.percent,
+        "Used": disk.used,
         "Free": disk.free,
         "Total": disk.total
     }
 
 if __name__ == "__main__":
     disk_info = check_disk_space()
-    # print(disk_info)
-    endpoint = "https://hooks.slack.com/services/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"
-    json_data = json.dumps(disk_info)
+    
+    endpoint = os.environ.get('SLACK_WEBHOOK_URI')
+    
+    # Create a Slack message attachment
+    attachment = {
+        "text": "Disk Space Usage",
+        "fields": [
+            {
+                "title": "Used",
+                "value": str(disk_info['Used']),
+                "short": True
+            },
+            {
+                "title": "Free",
+                "value": str(disk_info['Free']),
+                "short": True
+            },
+            {
+                "title": "Total",
+                "value": str(disk_info['Total']),
+                "short": True
+            }
+        ]
+    }
+    
+    # Create the main message payload
+    payload = {
+        "attachments": [attachment]
+    }
+
+    # Convert the payload to JSON format
+    json_data = json.dumps(payload)
+
     headers = {
         "Content-type": "application/json"
     }
 
     resp = requests.post(endpoint, data=json_data, headers=headers)
+
+    print(resp)
