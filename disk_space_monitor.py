888997a7d2ca79aa4ceaf1a69aca116fe057812c
kimanikevin254 <kimanikevin254@gmail.com>
2023-09-14 10:00:57 +0300

    more disk info

--- disk_space_monitor.py
+++ disk_space_monitor.py
@@ -2,7 +2,12 @@ import psutil
 
 def check_disk_space():
     disk = psutil.disk_usage('/var/lib/data')
-    return disk.percent
+    return {
+        "Used": disk.percent,
+        "Free": disk.free,
+        "Disks": disk.count,
+        "Total": disk.total
+    }
 
 if __name__ == "__main__":
     disk_percent = check_disk_space()
