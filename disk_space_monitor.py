f7c4cb23d622fdcefd2c1fe459aa90570e8332b8
kimanikevin254 <kimanikevin254@gmail.com>
2023-09-14 10:38:19 +0300

    attemp to post to slack

--- disk_space_monitor.py
+++ disk_space_monitor.py
@@ -1,14 +1,22 @@
 import psutil
+import requests
+import json
 
 def check_disk_space():
     disk = psutil.disk_usage('/var/lib/data')
     return {
         "Used": disk.percent,
         "Free": disk.free,
-        "Disks": disk.count,
         "Total": disk.total
     }
 
 if __name__ == "__main__":
-    disk_percent = check_disk_space()
-    print(f'Used disk space: {disk_percent}%')
+    disk_info = check_disk_space()
+    # print(disk_info)
+    endpoint = "https://hooks.slack.com/services/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"
+    json_data = json.dumps(disk_info)
+    headers = {
+        "Content-type": "application/json"
+    }
+
+    resp = requests.post(endpoint, data=json_data, headers=headers)
